9247446adff16b43fe33639efaa54b884a46ea52
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2022-10-31 14:55:35 +0000

    feat(*): init bot
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"gentoo-packages-bot/scraping"
+	"gentoo-packages-bot/handlers"
+	"gentoo-packages-bot/utils"
+	"log"
+	"os"
+	"time"
+
+	"gopkg.in/telebot.v3"
 )
 
 func main() {
-	fl := scraping.GetUseFlag("claasdasdg")
+	utils.LoadEnv(".env")
+	pref := telebot.Settings{
+		Token: os.Getenv("TELEGRAM_BOT_TOKEN"),
+		Poller: &telebot.LongPoller{
+			Timeout: 10 * time.Second,
+		},
+	}
 
-	fmt.Println(fl)
+	b, err := telebot.NewBot(pref)
+
+	b.Handle(telebot.OnQuery, handlers.OnQueryFunc)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	b.Start()
 }
